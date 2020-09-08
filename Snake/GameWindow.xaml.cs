6d16cc7442272ba48f9ec03e6943fca5d4eb22db
romalezgin1997 <romalezgin1997@gmail.com>
2020-09-08 22:47:52 +0400

    Add canvas

---
+++ Snake/GameWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Snake
+{
+    /// <summary>
+    /// Interaction logic for GameWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        public GameWindow()
+        {
+            InitializeComponent();
+            Rectangle head = new Rectangle();
+            head.RadiusX = 10;
+            head.RadiusY = 10;
+            canvas.Children.Add(head);
+        }
+    }
+}
